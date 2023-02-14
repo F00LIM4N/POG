7483a68306368ff30552abd0d93410d21ae605e8
Leo <leoaznargarrido@gmail.com>
2023-02-14 16:04:30 +0100

    update game

--- src/Entity/Game.php
+++ src/Entity/Game.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\GameRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -46,7 +48,7 @@ class Game
 
     #[ORM\Column]
     private ?bool $isAvailable = null;
-
+    
     public function getId(): ?int
     {
         return $this->id;
