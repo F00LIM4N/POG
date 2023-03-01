9210554f6c5a8199b419f513c8f4595f09065e30
Louison Delagarde <louison.delagarde@gmail.com>
2023-03-01 17:14:28 +0100

    reset password, envoie email dont work

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
