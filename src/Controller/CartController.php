21c05a026b3b350e610fd332961985fdfdf24c43
Louison <louison.delagarde@gmail.com>
2023-03-13 14:47:07 +0100

    page game + link vers home

---
+++ src/Controller/CartController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/cart')]
+class CartController extends AbstractController
+{
+    #[Route('/', name: 'app_cart')]
+    public function index(): Response
+    {
+        return $this->render('cart/index.html.twig', [
+            'controller_name' => 'CartController',
+        ]);
+    }
+
+    #[Route('/add/{id}', name: 'app_cart_add')]
+    public function add($id, SessionInterface $session): void
+    {
+    }
+}
