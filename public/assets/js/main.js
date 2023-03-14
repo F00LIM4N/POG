bc57b72ade8b011b665dc36fb1cb8ac3aa0e7c71
Louison Delagarde <louison.delagarde@gmail.com>
2023-03-14 22:35:18 +0100

    test logo + navbar

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,12 +1,20 @@
+const navbar = document.getElementById('navbar');
+const navbar_nav = document.getElementById('navbar_nav');
+const firstNavbarMid = document.querySelector('.FirstNavbarMid');
 
 
-window.addEventListener("scroll", () => {
-    if(window.scrollY>500) {
 
-        navbar.style.position = "fixed"
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 670) {
+        navbar.style.position="fixed";
+        navbar.style.width="100%";
+        navbar_nav.style.borderRadius="0px";
+        navbar.style.transition = 'all 0.5s ease-in-out'; 
     }
     else {
-        navbar.style.position = "static"
-
+        navbar.style.position="sticky";
+        navbar.style.width="820px";
+        navbar_nav.style.borderRadius="20px";
+        navbar.style.transition = 'all 0.5s ease-in-out';
     }
 })
